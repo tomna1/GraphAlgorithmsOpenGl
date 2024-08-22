d53166d21f9a99d346371fa98d369d58decbb267
tomna1 <thomas.e.nash@googlemail.com>
2024-08-22 19:49:01 +0100

    Bugfixes in MappedGraph class

--- include/MappedGraph.h
+++ include/MappedGraph.h
@@ -9,7 +9,7 @@
 /**
 * A graph of nodes and edges where the nodes have a mapped position (x and y
 * coordinate in a 2D space.
-* 
+*
 */
 class MappedGraph {
 private:
@@ -23,10 +23,10 @@ public:
 	// Initialises a graph with no nodes and no edges.
 	MappedGraph();
 	// Initialises a graph with the nodes in the list.
-	MappedGraph(const std::vector<GraphNode>&);
+	MappedGraph(const std::vector<GraphNode> &nodes);
 	// Initialises a graph with nodes and edges.
-	MappedGraph(const std::vector<GraphNode>& nodes,
-		const std::vector<std::vector<std::pair<GraphNode, int>>>& edges);
+	MappedGraph(const std::vector<GraphNode> &nodes,
+		const std::vector<std::vector<std::pair<GraphNode, int>>> &edges);
 	// Copy constructor.
 	MappedGraph(const MappedGraph &rhs);
 	// Destructor.
@@ -35,50 +35,58 @@ private:
 	// Returns the index of the specified node in the m_nodes array. Will
 	// return -1 if not found.
 	int getNodeIndex(const GraphNode &node);
-	// Returns the index of the specified edge in the vector which contains 
-	// the pairs of graphnodes and edgeweights. Returns -1 if the edge
-	// has not been found.
+	// Returns the index of the specified edge in the m_edges array. Returns 
+	// -1 if the edge has not been found.
 	int getEdgeIndex(const GraphNode &node, const GraphNode &rhs);
+	// Returns the node at m_nodes[index]. NO ERROR
+	// CHECKING SO MAKE SURE TO INPUT CORRECT INDEX.
+	GraphNode &getNode(const int index);
+	// Returns the node at m_edges[nodeIndex][edgeIndex]. NO ERROR CHECKING SO MAKE SURE TO INPUT CORRECT
+	// INDEX.
+	GraphNode &getNode(const int nodeIndex, const int edgeIndex);
 public:
 	// Returns the amount of nodes stored in the graph.
 	int getNodeCount();
 	// Returns the amount of edges stored in the graph.
 	int getEdgeCount();
+	// Gets the amount of edges a specific node in the graph has. Will return 
+	// -1 if the node is not found in the graph.
+	int getEdgeCount(const GraphNode &node);
+	// Gets the weight between of an edge between 2 nodes. If there is no edge
+	// then will return -1.
+	int getEdgeWeight(const GraphNode &node, const GraphNode &rhs);
+
+	// Checks if the graph has a certain node
+	bool hasNode(const GraphNode &node);
+	// Checks if the graph has a certain edge between nodes.
+	bool hasEdge(const GraphNode &node, const GraphNode &rhs);
+
 
 	// Adds Node to the graph. Returns true if nodes has been successfully added
 	// and false if the nodes has failed to be added.
-	bool addNode(const GraphNode&);
-	// Adds an edge between 2 nodes. Returns true if the edge has been successfully
-	// added and false if the edge has failed to be added.
-	bool addEdge(const GraphNode& node, const GraphNode& rhs, int weight);
-
+	bool addNode(const GraphNode &node);
+	// Adds a bidirectional edge between 2 nodes. Returns true if the edge has 
+	// been successfully added and false if the edge has failed to be added.
+	// Both specified nodes have to be in the graph for the edge to be added.
+	bool addEdge(const GraphNode &node, const GraphNode &rhs, int weight);
 	// Adds all the nodes to the graph. Will not add duplicate nodes. Returns
 	// true if all nodes not already in the graph have been added and false
 	// if all nodes not already in the graph have not all been successfully
 	// added.
 	bool addNodes(const std::vector<GraphNode> &nodes);
 	// Adds all the edges to a node.
-	bool addEdges(const GraphNode &node, const std::vector<std::pair<GraphNode, int>>& edges);
-
-	// Checks if the graph has a certain node
-	bool hasNode(const GraphNode&);
-	// Checks if the graph has a certain edge between nodes.
-	bool hasEdge(const GraphNode&, const GraphNode&);
+	bool addEdges(const GraphNode &node, const std::vector<std::pair<GraphNode, int>> &edges);
 
 	// Removes a nodes and all edges associated with that node.
-	bool removeNode(const GraphNode&);
-	// Removes an edge between 2 nodes.
-	bool removeEdge(const GraphNode&, const GraphNode&);
+	bool removeNode(const GraphNode &node);
+	// Removes an edge between 2 nodes. All edges are bidirectional so will
+	// remove the edge from both specified nodes.
+	bool removeEdge(const GraphNode &node, const GraphNode &rhs);
 	// Changes the edge weight of an edge between 2 nodes. Requires there to 
 	// already be an edge between 2 nodes.
-	bool changeEdgeWeight(const GraphNode&, const GraphNode&, const int newWeight);
-	// Gets the weight between of an edge between 2 nodes. If there is no edge
-	// then will return -1.
-	int getEdgeWeight(const GraphNode&, const GraphNode&);
+	bool changeEdgeWeight(const GraphNode &node, const GraphNode &rhs, const int newWeight);
 
-	// Returns the amount of edges a Node has.
-	int countConnections(const GraphNode&);
-	
+
 	// Print out and edge to the standard output. Example: Edge(Node(1, 2), 5)
 	// where 1 is the x coordinate of the node, 2 is the y coordinate and 5 is 
 	// the edge weight
@@ -98,19 +106,14 @@ public:
 	// Printing Edges of Node(5, 6)
 	// Edge(Node(1, 1), 4)
 	void printAllEdges();
-	
+
 	// Returns a list of nodes which indicate the shortest path between 2 nodes.
 	// A indicates this algorithm is the A* algorithm
-	std::vector<GraphNode> findShortestPathA(const GraphNode& start, const GraphNode& end);
+	std::vector<GraphNode> findShortestPathA(const GraphNode &start, const GraphNode &end);
 
 	// Returns a list of nodes which indicates the shortest path between 2 nodes.
 	// D indicates this uses Djikstra's shortest path algorithm.
 	std::vector<GraphNode> findShortestPathD(const GraphNode &start, const GraphNode &end);
 };
-
-
-
-
-
 
 #endif
--- src/MappedGraph.cpp
+++ src/MappedGraph.cpp
@@ -2,16 +2,18 @@
 
 #include "MappedGraph.h"
 
-
 // Initialises a graph with no nodes and no edges.
 MappedGraph::MappedGraph() {
 	m_nodes = {};
 	m_edges = {};
+	m_edges.resize(5);
 }
 // Initialises a graph with the nodes in the array and no edges.
 MappedGraph::MappedGraph(const std::vector<GraphNode> &nodes)
 	: m_nodes(nodes)
-{ }
+{
+	m_edges.resize(5);
+}
 // Initialises a graph with nodes and edges.
 MappedGraph::MappedGraph(const std::vector<GraphNode> &nodes,
 	const std::vector<std::vector<std::pair<GraphNode, int>>> &edges)
@@ -39,7 +41,7 @@ int MappedGraph::getEdgeIndex(const GraphNode &node, const GraphNode &rhs) {
 	if (index == -1) {
 		return -1;
 	}
-	
+
 	for (int i = 0; i < m_edges[index].size(); i++) {
 		if (std::get<0>(m_edges[index][i]) == rhs) {
 			return i;
@@ -47,6 +49,16 @@ int MappedGraph::getEdgeIndex(const GraphNode &node, const GraphNode &rhs) {
 	}
 	return -1;
 }
+GraphNode &MappedGraph::getNode(const int index) {
+	// if (index > m_nodes.size() - 1) return;
+	return m_nodes[index];
+}
+GraphNode &MappedGraph::getNode(const int nodeIndex, const int edgeIndex) {
+	// if (nodeIndex > m_nodes.size() - 1) return;
+	// if (edgeIndex > m_edges[nodeIndex].size() - 1) return;
+	return m_edges[nodeIndex][edgeIndex].first;
+}
+
 
 int MappedGraph::getNodeCount() {
 	return m_nodes.size();
@@ -58,10 +70,47 @@ int MappedGraph::getEdgeCount() {
 	}
 	return edgeCount;
 }
+int MappedGraph::getEdgeCount(const GraphNode &node) {
+	int index = getNodeIndex(node);
+	if (index == -1) return -1;
+
+	return m_edges[index].size();
+}
+int MappedGraph::getEdgeWeight(const GraphNode &node, const GraphNode &rhs) {
+	int index = getNodeIndex(node);
+	if (index == -1) return -1;
+
+	int edgeIndex = getEdgeIndex(node, rhs);
+	if (edgeIndex == -1) return -1;
+
+	return m_edges[index][edgeIndex].second;
+}
+
+
+bool MappedGraph::hasNode(const GraphNode &node) {
+	for (int i = 0; i < m_nodes.size(); i++) {
+		if (m_nodes[i] == node) return true;
+	}
+	return false;
+}
+bool MappedGraph::hasEdge(const GraphNode &node, const GraphNode &rhs) {
+	int index = getNodeIndex(node);
+	if (index == -1) return false;
+	int index2 = getNodeIndex(rhs);
+	if (index2 == -1) return false;
+
+	for (int i = 0; i < m_edges[index].size(); i++) {
+		if (std::get<0>(m_edges[index][i]) == rhs) {
+			return true;
+		}
+	}
+	return false;
+}
+
 
 bool MappedGraph::addNode(const GraphNode &node) {
 	m_nodes.push_back(node);
-	
+
 	if (m_nodes.size() >= m_edges.capacity()) {
 		m_edges.resize(m_edges.capacity() * 2);
 	}
@@ -69,13 +118,15 @@ bool MappedGraph::addNode(const GraphNode &node) {
 }
 bool MappedGraph::addEdge(const GraphNode &node, const GraphNode &to, int weight) {
 	int index = getNodeIndex(node);
-	if (index == -1) {
-		return false;
-	}
+	if (index == -1) return false;
+
+	int index2 = getNodeIndex(to);
+	if (index2 == -1) return false;
+
 	m_edges[index].push_back({ to, weight });
+	m_edges[index2].push_back({ node, weight });
 	return true;
 }
-
 bool MappedGraph::addNodes(const std::vector<GraphNode> &nodes) {
 	for (int i = 0; i < nodes.size(); i++) {
 		m_nodes.push_back(nodes[i]);
@@ -85,9 +136,7 @@ bool MappedGraph::addNodes(const std::vector<GraphNode> &nodes) {
 }
 bool MappedGraph::addEdges(const GraphNode &node, const std::vector<std::pair<GraphNode, int>> &edges) {
 	int index = getNodeIndex(node);
-	if (index == -1) {
-		return false;
-	}
+	if (index == -1) return false;
 
 	for (int i = 0; i < edges.size(); i++) {
 		if (!hasEdge(node, edges[i].first)) {
@@ -98,45 +147,30 @@ bool MappedGraph::addEdges(const GraphNode &node, const std::vector<std::pair<Gr
 	return true;
 }
 
-bool MappedGraph::hasNode(const GraphNode& node) {
-	for (int i = 0; i < m_nodes.size(); i++) {
-		if (m_nodes[i] == node) {
-			return true;
-		}
-	}
-	return false;
-}
-bool MappedGraph::hasEdge(const GraphNode& node, const GraphNode& rhs) {
-	int index = getNodeIndex(node);
-	if (index == -1) {
-		return false;
-	}
-	for (int i = 0; i < m_edges[index].size(); i++) {
-		if (std::get<0>(m_edges[index][i]) == rhs) {
-			return true;
-		}
-	}
-	return false;
-}
 
-bool MappedGraph::removeNode(const GraphNode& node) {
+bool MappedGraph::removeNode(const GraphNode &node) {
 	int index = getNodeIndex(node);
-	if (index == -1) {
-		return false;
-	}
-	std::vector<GraphNode>::iterator it_nodes = m_nodes.begin();
-	m_nodes.erase(it_nodes + index);
+	if (index == -1) return false;
 
-	std::vector<std::vector<std::pair<GraphNode, int>>>::iterator it_edges = m_edges.begin();
-	m_edges.erase(it_edges + index);
-	
+	while (m_edges[index].size() > 0) {
+		removeEdge(node, getNode(index, 0));
+	}
+
+	std::vector<GraphNode>::iterator itNodes = m_nodes.begin();
+	m_nodes.erase(itNodes + index);
+
+	std::vector<std::vector<std::pair<GraphNode, int>>>::iterator itEdges = m_edges.begin();
+	m_edges.erase(itEdges + index);
+
 	return true;
 }
-bool MappedGraph::removeEdge(const GraphNode& node, const GraphNode& rhs) {
+bool MappedGraph::removeEdge(const GraphNode &node, const GraphNode &rhs) {
 	int index = getNodeIndex(node);
-	if (index == -1) {
-		return false;
-	}
+	if (index == -1) return false;
+
+	int index2 = getNodeIndex(rhs);
+	if (index2 == -1) return false;
+
 	if (!hasEdge(node, rhs)) {
 		return false;
 	}
@@ -144,46 +178,31 @@ bool MappedGraph::removeEdge(const GraphNode& node, const GraphNode& rhs) {
 	int edgeIndex = getEdgeIndex(node, rhs);
 	std::vector<std::pair<GraphNode, int>>::iterator it = m_edges[index].begin();
 	m_edges[index].erase(it + edgeIndex);
+
+	int edgeIndex2 = getEdgeIndex(rhs, node);
+	it = m_edges[index2].begin();
+	m_edges[index2].erase(it + edgeIndex2);
+
 	return true;
 }
+
 
 bool MappedGraph::changeEdgeWeight(const GraphNode &node, const GraphNode &rhs, const int newWeight) {
 	int index = getNodeIndex(node);
-	if (index == -1) {
-		return false;
-	}
-	
+	if (index == -1) return false;
+
 	int edgeIndex = getEdgeIndex(node, rhs);
-	if (edgeIndex == -1) {
-		return false;
-	}
+	if (edgeIndex == -1) return false;
 
 	std::pair<GraphNode, int> newPair = std::make_pair(rhs, newWeight);
 	m_edges[index][edgeIndex] = newPair;
+
+	int index2 = getNodeIndex(rhs);
+	int edgeIndex2 = getEdgeIndex(rhs, node);
+	std::pair<GraphNode, int> newPair2 = std::make_pair(node, newWeight);
+	m_edges[index2][edgeIndex2] = newPair2;
+
 	return true;
-}
-
-int MappedGraph::getEdgeWeight(const GraphNode &node, const GraphNode &rhs) {
-	int index = getNodeIndex(node);
-	if (index == -1) {
-		return -1;
-	}
-
-	int edgeIndex = getEdgeIndex(node, rhs);
-	if (edgeIndex == -1) {
-		return -1;
-	}
-
-	return m_edges[index][edgeIndex].second;
-}
-
-int MappedGraph::countConnections(const GraphNode &node) {
-	int index = getNodeIndex(node);
-	if (index == -1) {
-		return -1;
-	}
-
-	return m_edges[index].size();
 }
 
 void MappedGraph::printEdge(const std::pair<GraphNode, int> &edge) {
@@ -198,10 +217,8 @@ void MappedGraph::printNodes() {
 }
 void MappedGraph::printEdges(const GraphNode &node) {
 	int index = getNodeIndex(node);
-	if (index == -1) {
-		return;
-	}
-	
+	if (index == -1) return;
+
 	std::cout << "Printing edges of ";
 	node.printNode();
 
@@ -210,7 +227,7 @@ void MappedGraph::printEdges(const GraphNode &node) {
 	}
 }
 void MappedGraph::printAllEdges() {
-	std::cout << "Printing all nodes of the graph" << std::endl;
+	std::cout << "Printing all Edges of the graph" << std::endl;
 	for (int i = 0; i < m_nodes.size(); i++) {
 		std::cout << "Printing Edges of ";
 		m_nodes[i].printNode();
@@ -231,4 +248,3 @@ void MappedGraph::printAllEdges() {
 //std::vector<GraphNode> MappedGraph::findShortestPathD(const GraphNode &start, const GraphNode &end) {
 	// TODO: THIS
 //}
-
